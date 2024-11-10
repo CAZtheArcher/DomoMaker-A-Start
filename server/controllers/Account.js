ae43ab7b2d56fc5a476fe89ec5277263034b22cb
CAZtheArcher <caz1158@g.rit.edu>
2024-11-10 17:12:09 -0500

    step 23 complete, error with signup timing out before completion

--- server/controllers/Account.js
+++ server/controllers/Account.js
@@ -1,6 +1,6 @@
 const models = require('../models');
 
-const { Account } = models.Account;
+const Account = models.Account;
 
 const loginPage = (req, res) => res.render('login');
 
@@ -9,7 +9,20 @@ const signupPage = (req, res) => res.render('signup');
 const logout = (req, res) => res.redirect('/');
 
 const login = (req, res) => {
+  const username = `${req.body.username}`;
+  const pass = `${req.body.pass}`;
 
+  if (!username || !pass) {
+    return res.status(400).json({ error: 'All fields are required!' });
+  }
+
+  return Account.authenticate(username, pass, (err, account) => {
+    if (err || !account) {
+      return res.status(401).json({ error: 'Wrong username or password!' });
+    }
+
+    return res.json({ redirect: '/maker' });
+  });
 };
 
 const signup = async (req, res) => {
